06ef375144a9b17f4ffb1eb40172b19ba2e00312
zunza <fksfks015@naver.com>
2023-04-04 14:54:02 +0900

    first commit
    Test commit

---
+++ src/main/java/com/example/nWebtoonAPI/NWebtoonApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nWebtoonAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NWebtoonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NWebtoonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nWebtoonAPI/NWebtoonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nWebtoonAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NWebtoonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
