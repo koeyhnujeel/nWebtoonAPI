fe204b250d51680fbc0c5ece737edd606e5ae17d
zunza <fksfks015@naver.com>
2023-04-04 15:51:46 +0900

    Create Episode.java
    episode entity

---
+++ src/main/java/com/example/nWebtoonAPI/domain/Episode.java
@@ -0,0 +1,47 @@
+package com.example.nWebtoonAPI.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "episode", schema = "nWebtoon")
+@NoArgsConstructor
+public class Episode {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "episode_id", unique = true, nullable = false)
+	private Long episodeId;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	@CreationTimestamp
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date updatedAt;
+
+	private double grade;
+
+	private String thumbUrl;
+
+	private String contentUrl;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "cartoon_id")
+	private Cartoon cartoon;
+}
